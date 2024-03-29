965e6d36b914c30f6e5d0d7f04c1c2a74f2d8eda
Jacob Higham <149442786+JakeHigham@users.noreply.github.com>
2024-03-28 23:02:13 -0500

    added config.js
    Config file set up to initialize sequalize, .env variables not defined

--- config/config.js
+++ config/config.js
@@ -1,15 +1,23 @@
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
+let sequelize;
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
+if (process.env.JAWSDB_URL) {
+  
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST || 'localhost', 
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+      port: process.env.DB_PORT || 3306, 
+    }
+  );
+}
 
 module.exports = sequelize;
