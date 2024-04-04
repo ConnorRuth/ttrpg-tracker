eaf5c96b9b1084e0c4c755b964420ad9586e2e94
ConnorRuth <152222829+ConnorRuth@users.noreply.github.com>
2024-04-03 21:04:06 -0700

    added hashing password limits

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/config.js');
+const bcrypt = require('bcrypt');
 class User extends Model {
 }
 
@@ -27,11 +28,24 @@ User.init(
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-            len: [8],
+            len: [8,16],
         },
     },
     },
+    
     {
+    hooks: {
+        beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        beforeUpdate: async (updatedUserData) => {
+            if (updatedUserData.password) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            }
+            return updatedUserData;
+        },
+          },
     sequelize,
     freezeTableName: true,
     modelName: 'user',
